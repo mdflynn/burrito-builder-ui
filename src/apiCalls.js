13164580fece5edc298577a55759e320b3c22bca
mdflynn <mdflynn34@outlook.com>
2021-01-19 09:54:36 -0700

    Add post

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,14 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+export const postOrder = (order) => {
+  fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(order)
+  })
+  .then(response => response.json())
+}
